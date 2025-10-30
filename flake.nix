f975372dd666bb6f9140eb352e1dbbd01fd26276
iAverages <me@danielraybone.com>
2025-10-30 16:50:06 +0000

    setup flake add kurumi host

--- flake.nix
+++ flake.nix
@@ -1,15 +1,37 @@
 {
-  description = "A very basic flake";
+  description = "homelab";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    inputs,
+    self,
+    nixpkgs,
+  }: let
+    hosts = {
+      "kurumi" = {};
+    };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    mkHost = hostname: attrs:
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+          system = "x86_64-linux";
+          meta = {
+            inherit hostname;
+            monitors = attrs.monitors;
+          };
+        };
+        modules = [
+          ./machines/${hostname}
+          ./modules/
+        ];
+      };
+  in {
+    nixosConfigurations =
+      hosts
+      |> nixpkgs.lib.mapAttrs (hostname: attrs: mkHost hostname attrs);
   };
 }
