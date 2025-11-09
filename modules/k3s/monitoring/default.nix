48293289381efbb09d43332f14c18099cb46aa9c
iAverages <me@danielraybone.com>
2025-11-09 14:16:50 +0000

    fix location of dashboards stuff

--- modules/k3s/monitoring/default.nix
+++ modules/k3s/monitoring/default.nix
@@ -1,3 +1,34 @@
-{
+{lib, ...}: {
   imports = [./prometheus-stack.nix];
+
+  services.k3s.manifests = let
+    dashboardDir = ./dashboards;
+    dashboards =
+      builtins.readDir dashboardDir
+      |> builtins.attrNames
+      |> builtins.filter (name: lib.hasSuffix ".json" name);
+
+    formatName = name: lib.removeSuffix ".json" name;
+  in
+    lib.genAttrs dashboards (
+      fileName: let
+        fullPath = "${dashboardDir}/${fileName}";
+        fileContent = builtins.readFile fullPath;
+        jsonContent = builtins.fromJSON fileContent;
+        dashboardName = formatName fileName;
+      in {
+        apiVersion = "v1";
+        kind = "ConfigMap";
+        metadata = {
+          name = "grafana-${dashboardName}-dashboard";
+          namespace = "monitoring";
+          labels = {
+            grafana_dashboard = "1";
+          };
+        };
+        data = {
+          "${fileName}" = jsonContent;
+        };
+      }
+    );
 }
--- modules/k3s/system/pihole/default.nix
+++ modules/k3s/system/pihole/default.nix
@@ -76,37 +76,6 @@ in {
       };
     };
 
-    manifests = let
-      dashboardDir = ./dashboards;
-      dashboards =
-        builtins.readDir dashboardDir
-        |> builtins.attrNames
-        |> builtins.filter (name: lib.hasSuffix ".json" name);
-
-      formatName = name: lib.removeSuffix ".json" name;
-    in
-      lib.genAttrs dashboards (
-        fileName: let
-          fullPath = "${dashboardDir}/${fileName}";
-          fileContent = builtins.readFile fullPath;
-          jsonContent = builtins.fromJSON fileContent;
-          dashboardName = formatName fileName;
-        in {
-          apiVersion = "v1";
-          kind = "ConfigMap";
-          metadata = {
-            name = "grafana-${dashboardName}-dashboard";
-            namespace = "monitoring";
-            labels = {
-              grafana_dashboard = "1";
-            };
-          };
-          data = {
-            "${fileName}" = jsonContent;
-          };
-        }
-      );
-
     secrets = [
       {
         name = "pihole-admin-password";
