a159684c8f16a09f1760f101e06af519db2e91e4
iAverages <me@danielraybone.com>
2025-11-15 00:30:29 +0000

    remove opt data from glance
    pod cannot see it

--- modules/k3s/applications/glance/default.nix
+++ modules/k3s/applications/glance/default.nix
@@ -47,9 +47,6 @@
                       "/" = {
                         hide = false;
                       };
-                      "/opt/data" = {
-                        hide = false;
-                      };
                     };
                   }
                 ];
