bda5bfb12fda34ca8af5cf65a960fc1a53bfcc35
iAverages <me@danielraybone.com>
2026-07-20 16:43:56 +0100

    feat: setup flux operator

--- modules/k3s/system/flux.nix
+++ modules/k3s/system/flux.nix
@@ -1,18 +1,47 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: let
   cfg = config.homelab.flux;
-  manifest = pkgs.runCommand "flux-install-${pkgs.fluxcd.version}.yaml" {} ''
-    ${pkgs.fluxcd}/bin/flux install --export > "$out"
-  '';
 in {
   options.homelab.flux.enable = lib.mkEnableOption "Flux controllers";
 
-  config.services.k3s.manifests.flux = lib.mkIf cfg.enable {
-    target = "flux.yaml";
-    source = manifest;
+  config.services.k3s.autoDeployCharts.flux-operator = lib.mkIf cfg.enable {
+    name = "flux-operator";
+    repo = "oci://ghcr.io/controlplaneio-fluxcd/charts/flux-operator";
+    version = "0.55.0";
+    hash = "sha256-NRZDB16X7BpNaP+LCh+50X/1SGt72/h37C27yv45JaI=";
+    targetNamespace = "flux-system";
+    createNamespace = true;
+
+    extraDeploy = [
+      {
+        apiVersion = "fluxcd.controlplane.io/v1";
+        kind = "FluxInstance";
+        metadata = {
+          name = "flux";
+          namespace = "flux-system";
+        };
+        spec = {
+          distribution = {
+            version = "2.8.x";
+            registry = "ghcr.io/fluxcd";
+          };
+          components = [
+            "source-controller"
+            "kustomize-controller"
+            "helm-controller"
+            "notification-controller"
+          ];
+          cluster = {
+            type = "kubernetes";
+            multitenant = false;
+            networkPolicy = true;
+            domain = "cluster.local";
+          };
+        };
+      }
+    ];
   };
 }
